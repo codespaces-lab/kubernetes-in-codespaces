ee35bd976ed624dd4f6aa8315ee699c2134e9e9f
josh <111506176+snff-io@users.noreply.github.com>
2023-07-11 05:22:11 +0000

    clean up

--- bl.metal/Agents/Spirit.cs
+++ bl.metal/Agents/Spirit.cs
@@ -12,13 +12,9 @@ public class Spirit : IEntity<Spirit>
 
     public IThink<Spirit> Previous { get; set; }
 
-    public Spirit()
-    {
-        //decivers are fine and get enough guff.
+    public Spirit();
 
-    }
-
-    public Task<IContext> Think(IContext context)
+    public Task<IContext> Think(IContext context, IConfiguration config)
     {
 
         if (context.map ? supervision_required == true : false)
@@ -28,16 +24,35 @@ public class Spirit : IEntity<Spirit>
                 active = 1,
                 actions_sec = 0,
                 receiver = 9999,
-                sender = new {
+                sender = new
+                {
                     id = 9999,
                     name = "supervisor",
-                    type = new {
+                    type = new
+                    {
                         name = "supervisor",
-                        id = 9999}
+                        id = 9999
+                    }
                 }
+            };
+            context.map.comms = new
+            {
+                active = 1,
+                default_level = 0,
+
+                in_filter = new[]
+                {
+
+                },
+                out_filter = new[]
+                {
+
+                }
+                //todo: reward system points can be spent to affect the spirit's own neural networks
             };
         }
         return context;
+
     }
 }
 
